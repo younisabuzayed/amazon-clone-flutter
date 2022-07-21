b57a34937b54874aff000168ed178580b5c53c98
younisabuzayed <younisabuzayed@gmail.com>
2022-07-21 21:26:25 +0300

    update 21-7

--- server/firebase-config.js
+++ server/firebase-config.js
@@ -6,7 +6,6 @@ var serviceAccount = require("./serviceAccountKey.json");
 
 adminFirebase.initializeApp({
   credential: adminFirebase.credential.cert(serviceAccount),
-  // databaseURL: "firebase-adminsdk-2yh9s@clone-eda6a.iam.gserviceaccount.com"
 })
 
 module.exports.adminFirebase = adminFirebase
